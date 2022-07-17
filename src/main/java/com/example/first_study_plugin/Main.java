de0526c4309bc87676399f21cd06c4de53984a13
n1sym <owlbook248@gmail.com>
2022-07-17 12:04:54 +0900

    chore: set to output "plugin enabled" log

--- src/main/java/com/example/first_study_plugin/Main.java
+++ src/main/java/com/example/first_study_plugin/Main.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
   @Override
   public void onEnable() {
+    getLogger().info("プラグインを有効にしました。");
     this.getCommand("greet").setExecutor(new CommandGreet());
   }
 }
