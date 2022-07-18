6784bb177d0a3946a7037f0190f437d72342f76e
n1sym <owlbook248@gmail.com>
2022-07-18 21:32:28 +0900

    feat: set seichi count when break block

--- src/main/java/com/example/first_study_plugin/SeichiAction.java
+++ src/main/java/com/example/first_study_plugin/SeichiAction.java
@@ -8,46 +8,58 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
+import org.bukkit.Bukkit;
+
 public class SeichiAction implements Listener {
 
-  public void rangeBreak(Block block, int[] range) {
+  public int rangeBreak(Block block, int[] range) {
     int i_start = range[0];
     int i_end = range[1];
     int k_start = range[2];
     int k_end = range[3];
 
+    int breaked_count = 0;
     for (int i = i_start; i <= i_end; i++) {
       for (int j = -1; j <= 1; j++) {
         for (int k = k_start; k <= k_end; k++) {
           Location location = block.getLocation();
           Block neighborhood_block = location.add(i, j, k).getBlock();
           if (neighborhood_block.getType() != Material.AIR) {
+            breaked_count += 1;
             neighborhood_block.breakNaturally();
           }
         }
       }
     }
+    return breaked_count;
   }
 
   @EventHandler
   public void onBlockBreak(BlockBreakEvent event) {
     Player player = event.getPlayer();
     String direction = Direction.getCardinalDirection(player);
-    //Bukkit.getLogger().info(direction);
 
+    int range[] = null; 
     Block block = event.getBlock();
     if (direction == "W") {
-      int range[] = { -1, 1, 0, 2 };
-      rangeBreak(block, range);
+      int t_range[] = { -1, 1, 0, 2 };
+      range = t_range;
     } else if (direction == "E") {
-      int range[] = { -1, 1, -2, 0 };
-      rangeBreak(block, range);
+      int t_range[] = { -1, 1, -2, 0 };
+      range = t_range;
     } else if (direction == "S") {
-      int range[] = { 0, 2, -1, 1 };
-      rangeBreak(block, range);
+      int t_range[] = { 0, 2, -1, 1 };
+      range = t_range;
     } else if (direction == "N") {
-      int range[] = { -2, 0, -1, 1 };
-      rangeBreak(block, range);
+      int t_range[] = { -2, 0, -1, 1 };
+      range = t_range;
     }
+
+    int breaked_count = rangeBreak(block, range);
+
+    Integer meta_count = SeichiLevel.getSeichiCount(player);
+    SeichiLevel.setSeichiCount(player, meta_count + breaked_count);
+    Bukkit.getLogger().info(" 整地カウント(meta): " + meta_count.toString());
+   
   }
 }
